ab95f2f4d21f50ad6c5965182d3684b7ba89ab2d
Derrick Bills <dbills777@gmail.com>
2021-08-21 16:25:18 -0600

    setup
    initial commit of tip calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './Styled.components/Header';
+import { DisplayBox } from './Styled.components/DisplayBox';
+import TipCalc from './Components/TipCalc';
+import { useState } from 'react';
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [bill, setBill] = useState('');
+  const [divisor, setDiviosr] = useState(1);
+
+  const calculateValues = () => {
+    setTotal(parseFloat(bill * (tip / 100) + bill));
+  };
+
+  const tipBtnClick = (tip, billAmount, divisor) => {
+    setDiviosr(parseInt(divisor));
+    setBill(parseFloat(billAmount));
+    setTip(parseFloat((tip / 100) * 100));
+    console.log((tip / 100) * billAmount);
+    calculateValues();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header>spli</Header>
+      <Header>tter</Header>
+      <DisplayBox>
+        <TipCalc
+          handleClick={tipBtnClick}
+          bill={bill}
+          totalPerPerson={total / divisor}
+          groupSize={divisor}
+          total={total}
+          tip={total - bill}
+          percent={tip}
+        />
+      </DisplayBox>
     </div>
   );
 }
